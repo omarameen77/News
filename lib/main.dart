2b056d1e094b1de5a979ad04d6d45e53201b281d
omarameen77 <omarxwork10@gmail.com>
2025-11-10 18:25:46 +0200

    feat: Add source selection and news display widgets
    - Implemented `SourceName` widget to display source names with selection styling.
    - Created `SourceTapWidget` to allow users to select different news sources and display corresponding news articles.
    - Developed `CategoryFragment` and `CategoryItems` for displaying categories of news with interactive elements.
    - Added `NewsItem` widget for rendering individual news articles with image, title, author, and publication time.
    - Introduced `NewsWidget` to fetch and display news articles based on selected sources.
    - Implemented `WebViewScreen` for displaying full articles in a web view.
    - Updated plugin registrations for URL launcher and web view functionalities across platforms.
    - Enhanced pubspec.yaml with new dependencies for caching, HTTP requests, and state management.
    - Added assets for images and fonts in the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:news/core/theme/app_theme.dart';
+import 'package:news/provider/theme_provider.dart';
+import 'package:news/screen/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const News());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const News(),
+    ),  );
 }
+
 
 class News extends StatelessWidget {
   const News({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      themeMode: themeProvider.isDarkMode?ThemeMode.dark:ThemeMode.light,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
